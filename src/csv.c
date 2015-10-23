4a2a1c94c5ab12e37a62906fd293763942aadd6c
Daniel Lovasko <daniel.lovasko@gmail.com>
2015-10-23 18:36:10 +0200

    various bugfixes and improvements

--- src/csv.c
+++ src/csv.c
@@ -1,73 +1,38 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdio.h>
 #include <fcntl.h>
+#include <errno.h>
 
 #include "csv.h"
 
 int
-csv_open(struct csv* file, char* path, char separator, size_t column_count)
+csv_open(struct csv* file, char* path, char separator, size_t field_count)
 {
 	if (file == NULL || path == NULL)
 		return CSV_E_NULL;
 
-	if (column_count == 0)
-		return CSV_E_COLUMN_COUNT;
+	if (field_count == 0)
+		return CSV_E_FIELD_COUNT;
 
 	if (separator == '\n')
 		return CSV_E_NEWLINE;
 
+	errno = 0;
 	file->fd = open(path, O_RDONLY);
 	if (file->fd < 0)
-		return CSV_E_PATH;
+		return CSV_E_IO;
 
-	file->cc = column_count;
+	file->field_count = field_count;
 	file->sep = separator;
-	file->fields = malloc(sizeof(char*) * column_count);
+	file->fields = malloc(sizeof(char*) * field_count);
 
 	file->i = 0;
 	file->bytes_read = 0;
 
-	memset(file->buffer, '\0', 65536);
+	memset(file->buffer, '\0', __CSV_BUF_SIZE);
 	file->fields[0] = &file->buffer[0];
-
-	return CSV_OK;
-}
-
-int
-csv_skip_line(struct csv* file, size_t n)
-{
-	char buffer[4096];
-	size_t skipped;
-	ssize_t buf_size;
-	ssize_t i;
-
-	if (file == NULL)
-		return CSV_E_NULL;
-
-	if (n == 0)
-		return CSV_OK;
-
-	skipped = 0;
-	while (skipped != n) {
-		buf_size = read(file->fd, buffer, 4096);
-
-		if (buf_size == -1)
-			return CSV_E_IO;
-
-		if (buf_size == 0)
-			return CSV_E_NOT_ENOUGH_LINES;			
-
-		for (i = 0; i < buf_size; i++) {
-			if (buffer[i] == '\n') {
-				skipped++;
-				if (skipped == n) {
-					lseek(file->fd, buf_size - i, SEEK_CUR);
-					return CSV_OK;
-				}
-			}
-		}
-	}
 
 	return CSV_OK;
 }
@@ -82,7 +47,7 @@ csv_read_record(struct csv* file, char*** out_fields)
 		return CSV_E_NULL;
 
 	if (file->i == file->bytes_read) {
-		file->bytes_read = read(file->fd, file->buffer, 65536);
+		file->bytes_read = read(file->fd, file->buffer, __CSV_BUF_SIZE);
 		if (file->bytes_read == -1)
 			return CSV_E_IO;
 
@@ -95,13 +60,13 @@ csv_read_record(struct csv* file, char*** out_fields)
 	file->fields[0] = &file->buffer[file->i];
 	fields = 1;
 	record_len = 0;
-	for (; file->i < file->bytes_read; file->i++) {
+	for (; file->i < file->bytes_read; file->i++, record_len++) {
 		if (file->buffer[file->i] == file->sep) {
 			file->buffer[file->i] = '\0';
 			file->fields[fields] = &file->buffer[file->i+1];
 			fields++;
 
-			if (fields == file->cc) {
+			if (fields == file->field_count) {
 				while (1) {
 					if (file->buffer[file->i] == '\n') {
 						file->buffer[file->i] = '\0';
@@ -117,8 +82,11 @@ csv_read_record(struct csv* file, char*** out_fields)
 					record_len++;
 
 					if (file->i == file->bytes_read) {
-						memset(file->buffer, '\0', 65536);
-						lseek(file->fd, -record_len, SEEK_CUR);
+						memset(file->buffer, '\0', __CSV_BUF_SIZE);
+						errno = 0;
+						if (lseek(file->fd, -record_len, SEEK_CUR) == -1)
+							return CSV_E_IO;
+
 						return csv_read_record(file, out_fields);
 					}
 				}
@@ -129,21 +97,41 @@ csv_read_record(struct csv* file, char*** out_fields)
 			return CSV_E_TOO_FEW_FIELDS;
 	}
 
+	if (fields < file->field_count) {
+		memset(file->buffer, '\0', __CSV_BUF_SIZE);
+		errno = 0;
+		if (lseek(file->fd, -record_len, SEEK_CUR) == -1)
+			return CSV_E_IO;
+
+		return csv_read_record(file, out_fields);
+	}
+
 	return CSV_OK;
 }
 
 int
 csv_error_string(int code, char** out_error_string)
 {
-	char* strings[] = {
+	static const char* strings[] = {
 		"OK",
-		"Null"
+		"End",
+		"Null pointer",
+		"Unexpected newline",
+		"Field count can not be zero",
+		"I/O error",
+		"Too few fields for record",
+		"Too many fields for record",
+		"Not enough lines to skip",
+		"Invalid return code"
 	};
+
 	if (out_error_string == NULL)
 		return CSV_E_NULL;
 
-	/* TODO define and check for CSV_E_MAX */
-	*out_error_string = strings[code];
+	if (code < 0 || code > CSV_E_MAX)
+		return CSV_E_INVALID_CODE;
+
+	*out_error_string = (char*)strings[code];
 	return CSV_OK;
 }
 
@@ -155,5 +143,7 @@ csv_close(struct csv* file)
 
 	free(file->fields);
 	close(file->fd);
+
+	return CSV_OK;
 }
 
--- src/csv.h
+++ src/csv.h
@@ -1,23 +1,24 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define CSV_OK                  0
-#define CSV_E_NULL              1
-#define CSV_E_NEWLINE           2
-#define CSV_E_COLUMN_COUNT      3
-#define CSV_E_ROW_LENGTH        4
-#define CSV_E_PATH              5
-#define CSV_E_UNKNOWN           6
-#define CSV_E_TOO_MANY_FIELDS   7
-#define CSV_E_IO                8
-#define CSV_E_NOT_ENOUGH_LINES  9
-#define CSV_E_TOO_FEW_FIELDS   10
-#define CSV_END                11
+#define CSV_OK                 0
+#define CSV_END                1
+#define CSV_E_NULL             2
+#define CSV_E_NEWLINE          3
+#define CSV_E_FIELD_COUNT      4
+#define CSV_E_IO               5
+#define CSV_E_TOO_FEW_FIELDS   6
+#define CSV_E_TOO_MANY_FIELDS  7
+#define CSV_E_NOT_ENOUGH_LINES 8
+#define CSV_E_INVALID_CODE     9
+#define CSV_E_MAX              9
+
+#define __CSV_BUF_SIZE 65536
 
 struct csv {
-	char buffer[65536];
+	char buffer[__CSV_BUF_SIZE];
 	char** fields;
-	size_t cc;
+	size_t field_count;
 	ssize_t i;
 	ssize_t bytes_read;
 	int fd;
@@ -25,9 +26,8 @@ struct csv {
 	char padding[3];
 };
 
-int csv_open(struct csv* file, char* path, char separator, size_t column_count);
-int csv_error_string(int code, char** out_error_string);
-int csv_skip_line(struct csv* file, size_t n);
+int csv_open(struct csv* file, char* path, char separator, size_t field_count);
 int csv_read_record(struct csv* file, char*** out_fields);
+int csv_error_string(int code, char** out_error_string);
 int csv_close(struct csv* file);
 
